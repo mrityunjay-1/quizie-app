2a3c3b3b60043e7350838dfe3e57a1e0ec6c50ae
Mrityujay Kumar <kumarmrityunjay7034@gmail.com>
2023-07-27 08:51:46 +0530

    Initial Commit

---
+++ quizie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quizie
+//
+//  Created by Mrityunjay Kumar on 27/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
